b9cec62bf243ed93b88926dc2a415e25aeee1186
Phong <phongduytran7999@gmail.com>
2020-08-02 21:07:15 +0700

    Controller (MVC)

--- BAI2/routes/user.route.js
+++ BAI2/routes/user.route.js
@@ -1,46 +1,18 @@
 var express = require('express');
-var shortid = require('shortid');
 
-var db = require('../db');
+var controller = require('../controllers/user.controller');
 
 var router = express.Router();
 
-router.get('/', function(req,res){
-	res.render('users/index', {
-		users: db.get('users').value()						
-	});
-});
+router.get('/', controller.index);
 
-router.get('/search', function(req,res){
-	var q = req.query.q;
-	var matchUsers = db.get('users').value().filter(function(users){
-		return users.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-	});
+router.get('/search', controller.search);
 
-	res.render('users/index',{
-		users: matchUsers
-	});
-});
+router.get('/create', controller.create);
 
-router.get('/create', function(req,res){
-	res.render('users/create');
-})
+router.get('/:id', controller.get);
 
-router.get('/:id', function(req,res){
-	var id = req.params.id;
-
-	var user = db.get('users').find({ id: id}).value();
-
-	res.render('users/view',{
-		user: user
-	});
-});
-
-router.post('/create', function(req,res){
-	req.body.id = shortid.generate();
-	db.get('users').push(req.body).write();
-	res.redirect('/users');
-})
+router.post('/create', controller.postCreate);
 
 
 module.exports = router;
